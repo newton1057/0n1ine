fae7fb79ec7c2c65b23e67c73231df6b39fc3c74
newton1057Mac <newton1057@gmail.com>
2022-09-23 19:50:02 -0500

    Se agregan notificaciones en Web

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import './Header.css'
 
 function Header(props) {
-  const [showAcceso, setAcceso] = useState(false)
+  const [showAcceso, setAcceso] = useState(true)
 
   return (
     <nav class="navbar navbar-expand-lg fixed-top">
--- src/Components/Tarjeta_Productos.js
+++ src/Components/Tarjeta_Productos.js
@@ -1,7 +1,20 @@
 import React from "react";
 import './Tarjeta_Productos.css'
+import toast, { Toaster } from 'react-hot-toast';
+
+
 
 function Tarjeta_Productos(props) {
+  
+  const notify = () => {
+    toast('Agregado a carrito!', {
+      duration: 1000,
+      position: 'top-center',
+      icon: '🛒',
+    });
+  }
+  
+
   return (
     <div className="Tarjeta_Productos">
         <div className="Tarjeta_Productos_Imagen">
@@ -12,8 +25,16 @@ function Tarjeta_Productos(props) {
             <h4>{props.Titulo}</h4>
             <h5>{props.Precio}</h5>
         </div>
-  
+
+        <div id="botones">
+          <button  class="btn btn-outline-header" onClick={notify}> Agregar al carrito </button>
+          <button  class="btn btn-outline-header"> Comprar ahora </button>
+        </div>
+        
+        <Toaster />
+
     </div>
+
   );
 }
 

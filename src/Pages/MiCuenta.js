edb2f8fa3ed6a3a8d04fd1a13528666a36838797
newton1057Mac <newton1057@gmail.com>
2022-09-24 19:45:15 -0500

    Se realiza Mi Cuenta

--- src/Pages/MiCuenta.js
+++ src/Pages/MiCuenta.js
@@ -3,24 +3,57 @@ import React, { useMemo, useState } from "react";
 import './MiCuenta.css';
 
 
-function Registrar() {
+function MiCuenta() {
     
 
   return (
 
-    <div>
+    <div className="MiCuenta">
         <h2>¡Hola Eduardo!</h2>
+      <div id="Conte">
 
-        <ol class="list-group list-group-numbered">
-            <li class="list-group-item">A list item</li>
-            <li class="list-group-item">A list item</li>
-            <li class="list-group-item">A list item</li>
-        </ol>
+      
+      <div class="card">
+        <div class="card-body" >
+        <i class="fa-solid fa-user-gear"></i> 
+        <h5 >General</h5>
+          <a href="#" class="btn btn-outline-header">Ir</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body" >
+        <i class="fa-solid fa-key"></i>
+        <h5 >Privacidad</h5>
+          <a href="#" class="btn btn-outline-header">Ir</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body" >
+        <i class="fa-solid fa-language"></i>
+        <h5 >Idioma</h5>
+          <a href="#" class="btn btn-outline-header">Ir</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body" >
+        <i class="fa-solid fa-bell"></i>
+        <h5 >Notificaciones</h5>
+          <a href="#" class="btn btn-outline-header">Ir</a>
+        </div>
+      </div>
 
+      <div class="card">
+        <div class="card-body" >
+        <i class="fa-solid fa-life-ring"></i>
+        <h5 >Notificaciones</h5>
+          <a href="#" class="btn btn-outline-header">Ir</a>
+        </div>
+      </div>
+      </div>
     </div>
 
     
   );
 }
 
-export default Registrar;
+export default MiCuenta;
